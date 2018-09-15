f816ef61b92c866cb6b9255c60c63f57f7ff5597
Jayson Fong <32722526+Jayson-Fong@users.noreply.github.com>
2018-09-15 19:29:44 -0400

    Removed Unneeded Return

--- upload/library/jayson/RAA/Extend/XenForo/Model/Avatar.php
+++ upload/library/jayson/RAA/Extend/XenForo/Model/Avatar.php
@@ -1,7 +1,7 @@
 <?php
 class jayson_RAA_Extend_XenForo_Model_Avatar extends XFCP_jayson_RAA_Extend_XenForo_Model_Avatar {
     public function applyAvatar($userId, $fileName, $imageType = false, $width = false, $height = false, $permissions = false) {
-        if ($imageType === IMAGETYPE_GIF && !XenForo_Visitor::getInstance()->hasPermission('avatar', 'jayson_raa_use_animated')) return $this->rejectAvatar();
+        if ($imageType === IMAGETYPE_GIF && !XenForo_Visitor::getInstance()->hasPermission('avatar', 'jayson_raa_use_animated')) $this->rejectAvatar();
         return parent::applyAvatar($userId, $fileName, $imageType, $width, $height, $permissions);
     }
     private function rejectAvatar() {
